427d46221ae67c06a41942870befeaf32f2b270c
Farouk Methkal <faroukmethkal@localhost.localdomain>
2020-06-25 18:53:36 +0200

    Updates

---
+++ precache-manifest.e2139b8c24f03aafd7d8afdec94f89d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "295f73c1965a3b251fbef6fef87f509e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2810ec1d6be1d5614017",
+    "url": "/monsters-rolodex/static/css/main.7129f04f.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2810ec1d6be1d5614017",
+    "url": "/monsters-rolodex/static/js/main.2799cea7.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
