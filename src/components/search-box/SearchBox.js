64f2db05df2cd90ad9c66e061431dd9a7f527a7e
Farouk Methkal <faroukmethkal@localhost.localdomain>
2020-06-25 18:46:34 +0200

    adding files for github

---
+++ src/components/search-box/SearchBox.js
@@ -0,0 +1,11 @@
+import './SearchBox.css'
+import React from 'react'
+
+export const SearchBox =({placeholder,handleChange})=>(
+    <input
+    className='search'
+          type="search"
+          placeholder={placeholder}
+          onChange={handleChange}
+        />
+)
